dbc7045c2aab6774af6920809f80594a0ece1efa
Mbedobe K <danielbushbaby@gmail.com>
2025-01-03 12:16:52 +0000

    template-created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-center text-red-600 ">
+        Welcome to my vite, react and tailwindcss project template
+      </h1>
+    </>
+  );
+}
+
+export default App;
